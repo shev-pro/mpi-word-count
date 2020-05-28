bf2b74874cd7c2545c01333f45072bf02b2cd795
Sergio Shevchenko <shevchenkser@gmail.com>
2020-05-28 22:06:25 +0200

    Minor fixes and refactoring

--- wc_core.h
+++ wc_core.h
@@ -9,21 +9,22 @@
 #include "wc_utils.h"
 #include "log.h"
 
-static void worker_process_files(struct LinkedList *local_file_list, int rank, enum wc_error *wc_status) {
-    ll_print(local_file_list);
+static struct WordFreq *worker_process_files(struct LinkedList *local_file_list, int rank, enum wc_error *wc_status) {
+    log_info("worker_process_files [local_file_list=%d, rank=%d] started", ll_size(local_file_list), rank);
+
     struct WordFreq *result = NULL;
 
     for (int i = 0; i < ll_size(local_file_list); i++) {
         char *path_to_count = ll_find(local_file_list, i)->data;
         result = word_frequencies(result, path_to_count, wc_status);
         if (NO_ERROR != *wc_status) {
-            log_error("Huston, we have an error with code %d", wc_status);
-            return;
+            log_error("Huston, we have an error with code %d", *wc_status);
+            return NULL;
         }
     }
-//    print_frequencies(result, true);
 
-    log_debug("Frequencies %d", rank);
+    log_info("worker_process_files [local_file_list=%d, rank=%d] finished", ll_size(local_file_list), rank);
+    return result;
 }
 
 #endif //MPI_WORD_COUNT_WC_CORE_H
